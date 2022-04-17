d626e038e6eb7520fbe2704178931292eb52985d
basachakoti <87137049+basachakoti@users.noreply.github.com>
2022-04-17 15:41:02 +0530

    initial

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="h3">
+    {{ message.message }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+  async fetch() {
+    this.message = await fetch("http://laravelsimpleapp-env.eba-n3xeje4c.us-east-1.elasticbeanstalk.com/sairam").then((res) =>
+      res.json()
+    );
+  },
+};
+</script>
